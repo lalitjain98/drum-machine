c760435c76edd51c8db0a4e9c033cc68730932a8
Lalit Jain <jain.lalit138@gmail.com>
2019-04-15 22:23:30 +0530

    Updates

---
+++ precache-manifest.1d45b9e9cc2c28285606f9a08d4c8f8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "afcf9fe73353caf758cf",
+    "url": "/drum-machine/static/js/runtime~main.27309cc5.js"
+  },
+  {
+    "revision": "c8774d11c271182366e2",
+    "url": "/drum-machine/static/js/main.aa8e257c.chunk.js"
+  },
+  {
+    "revision": "8a70600fc7fd61810736",
+    "url": "/drum-machine/static/js/2.5f344593.chunk.js"
+  },
+  {
+    "revision": "c8774d11c271182366e2",
+    "url": "/drum-machine/static/css/main.19b21aa6.chunk.css"
+  },
+  {
+    "revision": "088ad4df191d1886204930843b9a0dce",
+    "url": "/drum-machine/index.html"
+  }
+];
